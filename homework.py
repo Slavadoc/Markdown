21f344bf52828fb8f22eac758ac4aba9463ef425
Slavadoc <54536848+Slavadoc@users.noreply.github.com>
2022-09-17 22:43:43 +0300

    Fibonaci

--- homework.py
+++ homework.py
@@ -1,15 +1,18 @@
 # Kegel
-list = [int(i) for i in input().split()]
-print(list)
-for n in range(len(list)):
-    if N.count(list.[i]) == 1:
-        print(list[i], and = '.')
-# # Fibonici
-# array = [0,0]
-# array[0] = 0
-# array[1] = 1
-# N = int(input())
-#
-# for i in range(N):
-#     array.append(array[i] + array[i+1])
-# print("fibonati = ", array)
+# n,k = [int(i) for i in input().split()]
+# list = ['I'] * n
+# print(list)
+# for n in range(k):
+#     l,r = [int(s) for s in input().split()]
+#     for j in range(l-1,r):
+#         list[j] = '.'
+# print(' '.join(a))
+# Fibonici
+array = [0,0]
+array[0] = 0
+array[1] = 1
+N = int(input())
+
+for i in range(N):
+    array.append(array[i] + array[i+1])
+print("fibonati = ", array)
